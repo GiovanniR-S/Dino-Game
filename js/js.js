152eabecd9281c2020bead8d55c4593d48a013e6
GiovanniR-S <giovannirobertssilva@outlook.com>
2021-01-12 19:48:33 -0300

    Adicionando pontuação

--- js/js.js
+++ js/js.js
@@ -3,6 +3,13 @@ const background = document.querySelector(".background");
 let isJumping = false;
 let position = 0;
 
+let pontuação = 0;
+
+var pontos = document.getElementById("pontos");
+setInterval(() => {
+    pontos.innerHTML = '<h3 class = "pontos">Pontuação: '+ pontuação +'</h3>'
+}, 60);
+
 function handleKeyUp(event) {
     if(event.keyCode === 32){
         if(!isJumping){
@@ -40,21 +47,22 @@ function jump(){
 
 function createCactus(){
     const cactus = document.createElement('div');
-    let cactusPosition = 1000;
+    let cactusPosition = 1500;
     let randomTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
-    cactus.style.left = 1000 + "px";
+    cactus.style.left = 1500 + "px";
     background.appendChild(cactus);
 
     let leftIntervarl = setInterval(() => {
         if(cactusPosition < -60){
             clearInterval(leftIntervarl);
             background.removeChild(cactus);
+            pontuação++;
         }else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
             //Contado com o Dino
             clearInterval(leftIntervarl);
-            document.body.innerHTML = '<h1 class="gameOver">Fim de Jogo</h1>';
+            document.body.innerHTML = '<h1 class="gameOver">Fim de Jogo</h1>\n <h3 class = "pontuação">Pontuação: ' + pontuação +'</h3>\n <i class="fas fa-redo"></i>';
         } else{
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + "px";
